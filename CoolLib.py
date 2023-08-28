817573aeb31b537dcb9dc628bb980a5c2227dd49
Doster <63081538+Doster-d@users.noreply.github.com>
2023-08-29 02:42:58 +0300

    Initial commit

---
+++ CoolLib.py
@@ -0,0 +1,8 @@
+class CoolLib:
+	some_property = "Cool Lib"
+
+	def sum_int(self, f: int, s: int):
+		return f+s
+
+	def return_prop(self):
+		return self.some_property
---
+++ main.py
@@ -0,0 +1,8 @@
+from CoolLib import CoolLib
+
+if __name__ == "__main__":
+	cool = CoolLib()
+	print(
+		cool.sum_int(2, 5),
+		cool.return_prop()
+	)
